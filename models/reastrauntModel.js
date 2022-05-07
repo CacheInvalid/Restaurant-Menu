3b564a27269911b30d64f53da78b0fbd9d552345
CacheInvalid <59955366+CacheInvalid@users.noreply.github.com>
2022-05-07 10:45:33 +0100

    Update reastrauntModel.js

--- models/reastrauntModel.js
+++ models/reastrauntModel.js
@@ -20,6 +20,7 @@ class Menu
             description: 'You get a nice extra salty, but not salty in a bad way, taste addition to the burger mixed with the cheesy meltiness mixed in and you have a taste treat you cant deny',
             ingredients: 'Bacon, Cheese and Salt',
             allergy: 'This burger is not reccomended for anyone with Lactose issues',
+            chefPick: "true",
             price: '25.99'
         });
         //for later debugging
@@ -30,6 +31,7 @@ class Menu
             description: 'You get a nice extra salty, it also comes with a nice spicy taste that is perfect for those that love an extra kick',
             ingredients: 'Bacon, Cheese, Chili Peppers and Salt',
             allergy: 'This burger is not reccomended for anyone with Lactose issues',
+            chefPick: "true",
             price: '25.99'
         });
         //for later debugging
@@ -57,13 +59,14 @@ class Menu
         })
     }  
 
-    addEntry(name, description, ingredients, allergy) {
+    addEntry(name, description, ingredients, allergy, chef, price) {
         var entry = {
                 name: name,
                 description: description,
                 ingredients: ingredients,
                 allergy: allergy,
-                price: char().toISOString().split('T')[0]
+                chefPick: chef === "true",
+                price: price
                 }
         console.log('entry created', entry);
         this.db.insert(entry, function(err, doc) {
